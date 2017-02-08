29f8648adc41fd9dd5d9f00feb3be8cc499be68c
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-02-08 08:51:36 -0800

    adding guard connecting with db

--- app_client/addguard/addguardCtrl.js
+++ app_client/addguard/addguardCtrl.js
@@ -4,9 +4,43 @@
     .module('pingFyp')
     .controller('addguardCtrl', addguardCtrl);
 
-  
-  function addguardCtrl() {
+    addguardCtrl.$inject = ['authentication','multipartForm','$location'];
+
+  function addguardCtrl(authentication,multipartForm,$location) {
     var vm = this;
+
+    vm.credentials = {
+      name : "",
+      email : "",
+      code : "",
+      file:"",
+      phone:"",
+      home_address:""
+    };
+
+
+    vm.onSubmit = function () {
+      vm.formError = "";
+      if (!vm.credentials.name || !vm.credentials.home_address
+          || !vm.credentials.file || !vm.credentials.phone 
+          ||!vm.credentials.email || !vm.credentials.code
+          )
+           {
+        vm.formError = "All fields required, please try again";
+        return false;
+     
+      } else {
+        vm.doAddGuard();
+      }
+    };
+
+    vm.doAddGuard = function() {
+      vm.formError = "";
+     var uploadUrl = '/api/addingguard';
+    multipartForm.post(uploadUrl, vm.credentials).then(function(){
+          $location.path('/supervisor');
+        });
+    };
 
   
 
