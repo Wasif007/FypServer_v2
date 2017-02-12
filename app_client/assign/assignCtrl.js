6265de5b32a772896355552ed95e345ecc1a5c52
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-02-12 15:10:25 -0800

    debugging

--- app_client/assign/assignCtrl.js
+++ app_client/assign/assignCtrl.js
@@ -25,6 +25,24 @@
        assignedGuard.getListOfGuards()
        .success(function(data){
         vm.datas={guards:data};
+        vm.onSubmit=function()
+        {
+            if(!vm.credentials.supervisorName || !vm.credentials.guardName 
+    || !vm.credentials.supervisorImageUrl   
+    || !vm.credentials.time || !vm.credentials.location )
+
+{console.log("First Time "+vm.credentials.supervisorName +" "+vm.credentials.guardName 
+    +" "+vm.credentials.supervisorImageUrl  +" "+vm.credentials.guardImageUrl 
+    +" "+vm.credentials.time +" "+vm.credentials.location +" "+vm.credentials.guardUsername);
+           
+        vm.formError = "All fields required, please try again"
+         return false;
+     
+      } else
+      {
+ 
+      }
+          }
        })
        .error(function(e){
         console.log(e);
