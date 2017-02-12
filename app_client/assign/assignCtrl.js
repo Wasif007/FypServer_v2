cda2d6fa3672cc15180f4f91c28ccced69a5f373
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-02-12 13:13:09 -0800

    assign route in progress on front side

--- app_client/assign/assignCtrl.js
+++ app_client/assign/assignCtrl.js
@@ -28,9 +28,9 @@ vm.credentials = {
        });
 
        vm.onSubmit=function(){
-       if(!req.body.supervisorName || !req.body.guardName 
-    || !req.body.supervisorImageUrl  || !req.body.guardImageUrl 
-    || !req.body.time || !req.body.location ||!req.body.guardUsername)
+       if(!vm.credentials.supervisorName || !vm.credentials.guardName 
+    || !vm.credentials.supervisorImageUrl  || !vm.credentials.guardImageUrl 
+    || !vm.credentials.time || !vm.credentials.location ||!vm.credentials.guardUsername)
   
            {
         vm.formError = "All fields required, please try again"
@@ -46,7 +46,7 @@ vm.credentials = {
         vm.message="Sorry, something's gone wrong";
        });
       }
-        
+
        
        }
 
