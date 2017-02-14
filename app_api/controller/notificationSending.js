9366ec3b261fe1558abe1c64b805ff2b7a9b5946
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-02-13 21:29:57 -0800

    Route making of db with fcm

--- app_api/controller/notificationSending.js
+++ app_api/controller/notificationSending.js
@@ -1,10 +1,10 @@
 var mongoose=require('mongoose');
 var tokensFromClient=mongoose.model('assignTokens');
-var request=require('request');
-var FCM = require('fcm-push');
+//var request=require('request');
+//var FCM = require('fcm-push');
  
-var serverKey = 'AAAAKEUqUHQ:APA91bE12teqBTqSpZycw85IKyrGlzSAksoAWOlRI4NLRa51WjnM8acOmp5-KZRyEGw2L87Y0jfkSjwdPIzh4lL3owUz7bLw1JQAZL3v49r1Gg7km-DNheU1fP5ZF1IlaX6IdSONr7kT';
-var fcm = new FCM(serverKey);
+//var serverKey = 'AAAAKEUqUHQ:APA91bE12teqBTqSpZycw85IKyrGlzSAksoAWOlRI4NLRa51WjnM8acOmp5-KZRyEGw2L87Y0jfkSjwdPIzh4lL3owUz7bLw1JQAZL3v49r1Gg7km-DNheU1fP5ZF1IlaX6IdSONr7kT';
+//var fcm = new FCM(serverKey);
 
 
 var sendJSONresponse = function(res, status, content) {
@@ -14,6 +14,30 @@ var sendJSONresponse = function(res, status, content) {
 
 module.exports.gettingTokens=function(req,res)
 {
+if(!req.body.token)
+{
+  return sendJSONresponse(res,404,{
+    "Message":"Token value not provided"
+  });
+
+}
+else{
+  var tokenStoringInDb=new tokensFromClient();
+  tokenStoringInDb.Token=req.body.token;
+
+  tokenStoringInDb.save(function(err) {
+    if (err) {
+      sendJSONresponse(res, 404, err);
+    } else {
+      sendJSONresponse(res, 200, {
+        "token" : req.body.token
+      });
+    }
+  });
+}
+}
+
+  /*
 var message = {
     to: req.body.token, // required fill with device token or topics
     notification: {
@@ -36,36 +60,8 @@ fcm.send(message, function(err, response){
     }
 });
 
-	/* if(!req.body.token)
-   {
-    return sendJSONresponse(res,404,{
-      "Message":"Provide Token "
-    });
-   }
-   else
-   {
-  var options={
-uri: 'https://fcm.googleapis.com/fcm/send',
-    method: 'POST',
-    headers: { 'Authorization': 'key=' + 'AAAAKEUqUHQ:APA91bE12teqBTqSpZycw85IKyrGlzSAksoAWOlRI4NLRa51WjnM8acOmp5-KZRyEGw2L87Y0jfkSjwdPIzh4lL3owUz7bLw1JQAZL3v49r1Gg7km-DNheU1fP5ZF1IlaX6IdSONr7kT' },
-         json: {
-      // note that Sequelize returns token object array, we map it with token value only
-      'to':"f-t20WoRimA:APA91bHnED-RgVoUhZGj7o09dWK2XB_vs5ju9lGp5J422lzIP3h8p4NZgyNOtAEQrJAps4cZoJLn86bba5SEaQoRAYVT7UBhhqG0j4lkmjc7uzDp6Tavhp-Z3qIJgrvnghc7CUgaCcRX",
-      'data': { 'title': "Hello", 'body': 'Naeem Bhai' }
-    }
-          };
-  request(options, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      // request was success, should early return response to client
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(500);
-    }
-    
-  });
-    }
-   */
-}
+	*/
+
 
 module.exports.gettingTokensFromDb=function(req,res)
 {
