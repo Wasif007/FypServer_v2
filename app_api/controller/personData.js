49bc3df600515d376b631303fbb9c3b48d239bea
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-02-13 21:44:12 -0800

    fcm routes in progress

--- app_api/controller/personData.js
+++ app_api/controller/personData.js
@@ -1,3 +1,9 @@
+var tokensFromClient=mongoose.model('assignTokens');
+
+var FCM = require('fcm-push');
+ 
+var serverKey = 'AAAAKEUqUHQ:APA91bE12teqBTqSpZycw85IKyrGlzSAksoAWOlRI4NLRa51WjnM8acOmp5-KZRyEGw2L87Y0jfkSjwdPIzh4lL3owUz7bLw1JQAZL3v49r1Gg7km-DNheU1fP5ZF1IlaX6IdSONr7kT';
+var fcm = new FCM(serverKey);
 
 
 var sendJSONresponse = function(res, status, content) {
@@ -7,10 +13,21 @@ var sendJSONresponse = function(res, status, content) {
 
 module.exports.gettingDataPerson=function(req,res)
 {
+  var query = tokensFromClient.find({}).select('token -_id');
+
+    query.exec(function (err, someValue) {
+        if (err) return next(err);
+        else
+        {
+        	sendJSONresponse(res,200,{
+        		"Message":someValue
+        	})
+        }
+    });
   if(req.body.name)
 {
  return sendJSONresponse(res,200,{
-    "Message":req.body.name+" Hello Sex"
+    "Message":req.body.name+" Hello "
   })
 }
 else
