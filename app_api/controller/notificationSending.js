b0bcde0bf91c5b7b8d115d7cc962b8ef79560447
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-02-13 18:50:44 -0800

    fcm

--- app_api/controller/notificationSending.js
+++ app_api/controller/notificationSending.js
@@ -6,14 +6,7 @@ var FCM = require('fcm-push');
 var serverKey = 'AAAAKEUqUHQ:APA91bE12teqBTqSpZycw85IKyrGlzSAksoAWOlRI4NLRa51WjnM8acOmp5-KZRyEGw2L87Y0jfkSjwdPIzh4lL3owUz7bLw1JQAZL3v49r1Gg7km-DNheU1fP5ZF1IlaX6IdSONr7kT';
 var fcm = new FCM(serverKey);
 
-var message = {
-    to: "f-t20WoRimA:APA91bHnED-RgVoUhZGj7o09dWK2XB_vs5ju9lGp5J422lzIP3h8p4NZgyNOtAEQrJAps4cZoJLn86bba5SEaQoRAYVT7UBhhqG0j4lkmjc7uzDp6Tavhp-Z3qIJgrvnghc7CUgaCcRX", // required fill with device token or topics
-    
-    notification: {
-        title: 'Title of your push notification',
-        body: 'Body of your push notification'
-    }
-};
+
 var sendJSONresponse = function(res, status, content) {
   res.status(status);
   res.json(content);
@@ -21,25 +14,26 @@ var sendJSONresponse = function(res, status, content) {
 
 module.exports.gettingTokens=function(req,res)
 {
-
+var message = {
+    to: req.body.token, // required fill with device token or topics
+    
+    notification: {
+        title: 'Naeem',
+        body: 'Bhai'
+    }
+};
 //callback style
 fcm.send(message, function(err, response){
     if (err) {
         console.log("Something has gone wrong!");
     } else {
         console.log("Successfully sent with response: ", response);
+    sendJSONresponse(res,200,{
+      "Message":" FCM"
+    })
     }
 });
 
-//promise style
-fcm.send(message)
-    .then(function(response){
-        console.log("Successfully sent with response: ", response);
-    })
-    .catch(function(err){
-        console.log("Something has gone wrong!");
-        console.error(err);
-    })
 	/* if(!req.body.token)
    {
     return sendJSONresponse(res,404,{
