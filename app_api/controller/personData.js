db7d3a3f19bc3e3c282a5cd9580f7965d6f97e8f
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-02-15 01:52:05 -0800

    token changed

--- app_api/controller/personData.js
+++ app_api/controller/personData.js
@@ -6,7 +6,7 @@ var tokensFromClient=mongoose.model('assignTokens');
 
 var FCM = require('fcm-push');
  
-var serverKey = 'AAAAKEUqUHQ:APA91bE12teqBTqSpZycw85IKyrGlzSAksoAWOlRI4NLRa51WjnM8acOmp5-KZRyEGw2L87Y0jfkSjwdPIzh4lL3owUz7bLw1JQAZL3v49r1Gg7km-DNheU1fP5ZF1IlaX6IdSONr7kT';
+var serverKey = 'AAAAHSygsvw:APA91bEqTlPBHbiov0G-dVs2KAXMprCm9bSHrYusLQHpnQI84ZuJgmfGAz_Nz_0he4yHKza0v7q_eyRwAKWovCiTLfyzjNvDOskXEoeyJ6qrPyLFeSSGXYBF0S48XkoXVOfGUpeLfuOF';
 var fcm = new FCM(serverKey);
 
 
@@ -25,7 +25,7 @@ var value;
         {
           value=someValue[0].Token;
           var message = {
-    "to": value,
+    "to": 'cHrhAho78mc:APA91bGmJBA5WzWLMd4Vv1oC5RpPR7ybc6twrNRXmwz2vVhHO_6HXWo5A9TRV-Vxpjj-upA1BXZ0yhRVXrxKfD7pqtKADHpKyy7rEuKKrHsQYMWm0cIfx-Q0eM_uDXo7399TOFEGv3We',
     
     "notification" : {
         "title" : 'Title of the notification',
