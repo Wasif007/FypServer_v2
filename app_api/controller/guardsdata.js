e89b6597e342d49fcea24774e9255da70f8d2808
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-04-03 12:08:31 -0700

    error handling

--- app_api/controller/guardsdata.js
+++ app_api/controller/guardsdata.js
@@ -12,6 +12,7 @@ var sendJSONresponse = function(res, status, content) {
 
 module.exports.gettingData=function(req,res)
 {
+  console.log(req.params.guardName);
 	 if(req.params.guardName)
   {
   Guard.findOne({name:req.params.guardName}, function(err, docs) {
