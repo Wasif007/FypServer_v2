7afd88f99c4d90046f6b7e58fba7561aa49eed94
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-04-03 16:34:58 -0700

    error handling

--- app_api/controller/guardsdata.js
+++ app_api/controller/guardsdata.js
@@ -114,7 +114,7 @@ module.exports.guardsAssigning=function(req,res)
 {
 	var image_url_cloudinary;
     image_url_cloudinary=req.file.url;
-            if(req.file.url===undefined || req.file.url===null)
+            if(req.file===undefined || req.file===null)
             {
               return sendJSONresponse(res,401,{
                 "Message":"Provide all required fields"
