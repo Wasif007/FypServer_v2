19519db02e7e8e02ae9f9ce2cb91f19857f3416a
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-04-05 13:46:50 -0700

    debugging

--- app_api/controller/notificationSending.js
+++ app_api/controller/notificationSending.js
@@ -1,11 +1,5 @@
 var mongoose=require('mongoose');
 var tokensFromClient=mongoose.model('assignTokens');
-//var request=require('request');
-//var FCM = require('fcm-push');
- 
-//var serverKey = 'AAAAKEUqUHQ:APA91bE12teqBTqSpZycw85IKyrGlzSAksoAWOlRI4NLRa51WjnM8acOmp5-KZRyEGw2L87Y0jfkSjwdPIzh4lL3owUz7bLw1JQAZL3v49r1Gg7km-DNheU1fP5ZF1IlaX6IdSONr7kT';
-//var fcm = new FCM(serverKey);
-
 
 var sendJSONresponse = function(res, status, content) {
   res.status(status);
@@ -16,8 +10,8 @@ module.exports.gettingTokens=function(req,res)
 {
 if(!req.body.token)
 {
-  return sendJSONresponse(res,404,{
-    "Message":"Token value not provided"
+  return sendJSONresponse(res,401,{
+    "Message":"Required fields are not provided"
   });
 
 }
@@ -27,7 +21,7 @@ else{
 
   tokenStoringInDb.save(function(err) {
     if (err) {
-      sendJSONresponse(res, 404, err);
+      sendJSONresponse(res, 401, err);
     } else {
       sendJSONresponse(res, 200, {
         "token" : req.body.token
@@ -37,30 +31,7 @@ else{
 }
 }
 
-  /*
-var message = {
-    to: req.body.token, // required fill with device token or topics
-    notification: {
-        title: 'Person Identication'
-    },
-    data: {
-        title: 'Naeem',
-        body: 'Bhai'
-    }
-};
-//callback style
-fcm.send(message, function(err, response){
-    if (err) {
-        console.log("Something has gone wrong!");
-    } else {
-        console.log("Successfully sent with response: ", response);
-    sendJSONresponse(res,200,{
-      "Message":" FCM"
-    })
-    }
-});
-
-	*/
+  
 
 
 module.exports.gettingTokensFromDb=function(req,res)
@@ -81,7 +52,7 @@ sendJSONresponse(res,200,docs);
     if(!err)
     {
      sendJSONresponse(res,200,{
-  "Message":" Deleted all data"
+  "Message":"Deleted all data"
 })
     }
 
