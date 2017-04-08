cf9cd9a5543ceed25e6a52d81684ee0b6230240d
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-04-08 15:47:50 -0700

    debugging

--- app_client_2/index.js
+++ app_client_2/index.js
@@ -1,9 +1,6 @@
 /*globals $*/
 
-function isValidEmailAddress(emailAddress) {
-    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-    return pattern.test(emailAddress);
-};
+
  $(".form input#lg").click(function (e) {
     e.preventDefault();
 $('#names').text('');
@@ -13,16 +10,17 @@ $('#names').text('Email Required');
   return ;
 }
 
-  if( !isValidEmailAddress(('.form input#email ').val())) { 
-   $('#names').text('Provide Valid Email'); 
-  return ;
-   }
+  
 
   if( $('.form input#p ').val().length === 0 ) {
 $('#names').text('Password Required'); 
   return ;
 }
-    
+    var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+if (!testEmail.test($('.form input#p ').val()))
+{ $('#names').text('Valid Email Required'); 
+  return ;
+}
    var dataJson= {
 	"email":$('.form input#email ').val(),
 	"password":$('.form input#p ').val()
