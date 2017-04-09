8b72365d1b81f9e0fd30846660f4e1ba1d7f2068
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-04-08 18:34:44 -0700

    debugging

--- app_client_2/assign.js
+++ app_client_2/assign.js
@@ -29,7 +29,7 @@ $.getJSON($('.col-1-3 select#guards').attr("action"), function (data) {
   
   
    var dataJson= {
-	"supervisorName":localStorage.getItem('supervisor_Name'),
+	"supervisorName":'',
 	"guardName":$('.col-1-3 select#guards ').val(),
 	"location":$('.col-1-3 select#location ').val(),
 	"guardImageUrl":$('.col-1-3 img').attr("src"),
